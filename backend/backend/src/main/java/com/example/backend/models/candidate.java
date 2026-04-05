90c75cd2483ad5ec3c2bc705f13039e242459e4c
maaajaaaa <62818220+maaajaaaa@users.noreply.github.com>
2026-04-06 01:00:40 +0200

    Created Models

--- backend/backend/src/main/java/com/example/backend/models/candidate.java
+++ backend/backend/src/main/java/com/example/backend/models/candidate.java
@@ -7,7 +7,47 @@ public class candidate {
     private String name;
     private String email;
     private Date date_of_birth;
-    private String number;
+    private String contact_number;
+
+    public candidate(int id, String name, String email, Date date_of_birth, String contact_number) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.date_of_birth = date_of_birth;
+        this.contact_number = contact_number;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+    public String getContact_number() {
+        return contact_number;
+    }
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    
 
 
 }
--- backend/backend/src/main/java/com/example/backend/models/candidateskill.java
+++ backend/backend/src/main/java/com/example/backend/models/candidateskill.java
@@ -1,5 +1,26 @@
 package com.example.backend.models;
 
 public class candidateskill {
+    private int candidate_id;
+    private int skill_id;
+
+    
+    public candidateskill(int candidate_id, int skill_id) {
+        this.candidate_id = candidate_id;
+        this.skill_id = skill_id;
+    }
+    public int getCandidate_id() {
+        return candidate_id;
+    }
+    public void setCandidate_id(int candidate_id) {
+        this.candidate_id = candidate_id;
+    }
+    public int getSkill_id() {
+        return skill_id;
+    }
+    public void setSkill_id(int skill_id) {
+        this.skill_id = skill_id;
+    }
+
     
 }
